8aa8790e0f1e0a5e59ccc9ea59bf5e32fec93acc
Viraat Das <viraat.laldas@gmail.com>
2020-01-29 18:25:02 -0500

    milestone 1

---
+++ Tumblr/PhotosViewController.swift
@@ -0,0 +1,58 @@
+//
+//  PhotosViewController.swift
+//  Tumblr
+//
+//  Created by Viraat Das on 1/29/20.
+//  Copyright © 2020 Viraat Das. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+    var posts: [[String: Any]] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        // Network request snippet
+        let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        session.configuration.requestCachePolicy = .reloadIgnoringLocalCacheData
+        let task = session.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data,
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                
+                
+                
+                
+                // TODO: Get the posts and store in posts property
+                let responseDictionary = dataDictionary["response"] as! [String: Any]
+                self.posts = responseDictionary["posts"] as! [[String: Any]]
+                
+                
+                
+                // TODO: Reload the table view
+            }
+        }
+        task.resume()
+        
+        
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
